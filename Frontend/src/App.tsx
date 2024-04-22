a4d74eaed4932293ce777e31b13dae0c9f63f92b
alexDanilMain <alexdanilwork@gmail.com>
2024-04-22 10:43:50 +0200

    Add google login

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import { GoogleLogin } from "@react-oauth/google";
 
 function App() {
 
   return (
-    <>
-    <h1>Hello World!</h1>
-    </>
+    <GoogleLogin
+    onSuccess={credentialResponse => {
+        const expirationDate = new Date();
+        console.log("entered")
+        document.cookie = `test_key=${credentialResponse.credential};`
+    }}
+    onError={() => {
+      console.log('Login Failed');
+    }}
+  />
   )
+  
 }
 
 export default App
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId="1021052820543-fm1vrkkpkq1idpvckttevn0ir9d9qdc2.apps.googleusercontent.com">
+
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </GoogleOAuthProvider>
 )
