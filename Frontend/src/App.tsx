fe3196173ecde6282fd7876d17b2b709677f3c6a
Ewy Rostam <ewy.rost@gmail.com>
2024-04-22 11:01:30 +0200

    Create new api calendar

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,18 @@
 import { GoogleLogin } from "@react-oauth/google";
+import ApiCalendar from "react-google-calendar-api";
 
 function App() {
+
+  const config = {
+    clientId: "1021052820543-fm1vrkkpkq1idpvckttevn0ir9d9qdc2.apps.googleusercontent.com",
+    apiKey: "AIzaSyCofiE5jlHTtCJxtaLFRqoym3lAdpR6qKs",
+    scope: "https://www.googleapis.com/auth/calendar",
+    discoveryDocs: [
+      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+    ],
+  }
+
+  const apiCalendar = new ApiCalendar(config);
 
   return (
     <GoogleLogin
