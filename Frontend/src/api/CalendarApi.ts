c3a216715b39ad572fd7fc8f06f43437061cc78d
Ewy Rostam <ewy.rost@gmail.com>
2024-04-24 13:13:13 +0200

    Add function to post hardcoded reoccuring event

--- Frontend/src/api/CalendarApi.ts
+++ Frontend/src/api/CalendarApi.ts
@@ -3,7 +3,8 @@ import { getCookie } from "../helpers/CookieHelpers";
 
 const now = new Date();
 const newTime = addHours(now, 2);
-const BASE_URL = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
+const BASE_URL =
+  "https://www.googleapis.com/calendar/v3/calendars/primary/events";
 
 export async function createCalendarEvent() {
   const event = {
@@ -19,38 +20,66 @@ export async function createCalendarEvent() {
     },
   };
 
-  await fetch(
-    BASE_URL,
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        Authorization: `Bearer ${getCookie("access_token")}`,
-      },
-      body: JSON.stringify(event),
-    }
-  ).then((data) => {
+  await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${getCookie("access_token")}`,
+    },
+    body: JSON.stringify(event),
+  }).then((data) => {
+    alert("Event created, check your Google Calendar!");
+    return data.json();
+  });
+}
+
+export async function createCalendarTemplate() {
+  const event = {
+    summary: "Coding Bootcamp Demo",
+    location: "Bootcamp Venue, Stockholm",
+    start: {
+      dateTime: "2024-06-02T15:00:00+02:00",
+      timeZone: "Europe/Stockholm",
+    },
+    end: {
+      dateTime: "2024-06-02T16:00:00+02:00",
+      timeZone: "Europe/Stockholm",
+    },
+    recurrence: ["RRULE:FREQ=WEEKLY;COUNT=11;BYDAY=MO"],
+    extendedProperties: {
+        shared: {
+          template: "salt"
+        }
+      }
+  };
+
+  await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${getCookie("access_token")}`,
+    },
+    body: JSON.stringify(event),
+  }).then((data) => {
     alert("Event created, check your Google Calendar!");
     return data.json();
   });
 }
 
 export async function deleteCalendarEvent(eventId: string) {
-
-    await fetch(
-        BASE_URL + `/${eventId}?key=${import.meta.env.VITE_APP_API_KEY}`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `Bearer ${getCookie("access_token")}`,
-          },
-        }
-      ).then((data) => {
-        alert("Event deleted, check your Google Calendar!");
-        return data.json();
-      });
-
+  await fetch(
+    BASE_URL + `/${eventId}?key=${import.meta.env.VITE_APP_API_KEY}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${getCookie("access_token")}`,
+      },
+    }
+  ).then((data) => {
+    alert("Event deleted, check your Google Calendar!");
+    return data.json();
+  });
 }
 
-
+export async function deleteTemplate(template: string) {}
