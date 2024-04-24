ed8978fe1450a1ffec33846f8bcec1e2d19f4b8a
alexDanilMain <alexdanilwork@gmail.com>
2024-04-24 15:34:20 +0200

    adds weeks usestate

--- Frontend/src/components/Home/CreateTemplate/CreateTemplate.tsx
+++ Frontend/src/components/Home/CreateTemplate/CreateTemplate.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import WeekTable from "./WeekTable/WeekTable"
 
+type Week = {
+    name:string;
+    id: string;
+}
 function CreateTemplate() {
+    const [weeks, setWeeks] = useState<Week[]>([]); // State to manage weeks
+
+    const handleAddWeek = () => {
+        const newWeek = {name:"test", id:"test"}; // You can initialize your new week with any default values
+        setWeeks((prevWeeks) => [...prevWeeks, newWeek]); // Add new week to the weeks array
+    };
     return (
         <section className="px-4">
             <form action="">
-                <input type="button" name="addWeek" />
-                <label htmlFor="addWeek" className="btn btn-sm">+ Add Week</label>
+                <button type="button" onClick={handleAddWeek} className="btn btn-sm">+ Add Week</button>
                 <WeekTable />
+
+                <input type="submit" className="btn btn-sm mt-4"  value="Create Template"/>
             </form>
 
           
