5336e70f6d1d2783ef240f734589da548c942e9e
Axel Petersson <axel.818@gmail.com>
2024-04-25 12:14:50 +0200

    adds dbcontext

---
+++ backend/dbContext.cs
@@ -0,0 +1,15 @@
+public class AppDbContext : DbContext
+{
+    public DbSet<Template> Templates { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Template>().ToTable("Templates");
+        modelBuilder.Entity<CalendarEvent>().ToTable("CalendarEvents");
+        modelBuilder.Entity<EventDateTime>().ToTable("EventDateTimes");
+
+        // Configure relationships and more complex constraints here
+    }
+}
