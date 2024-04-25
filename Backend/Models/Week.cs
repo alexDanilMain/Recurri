202d803091df04b2a9118c940ce546439d711997
Ewy Rostam <ewy.rost@gmail.com>
2024-04-25 22:48:02 +0200

    Add fields to week model

--- Backend/Models/Week.cs
+++ Backend/Models/Week.cs
@@ -2,6 +2,8 @@ namespace Backend.Models
 {
     public class Week
     {
-        
+        public int Id { get; set; }
+        public int Number {get; set;}
+        public Event[] Events {get; set;} = [];
     }
 }
