1eb6dbd52c06173394e167daa287f2949439392a
Ewy Rostam <ewy.rost@gmail.com>
2024-04-26 00:35:35 +0200

    Create post method for template

--- Frontend/src/api/TemplateApi.ts
+++ Frontend/src/api/TemplateApi.ts
@@ -1,3 +1,27 @@
+import { Week } from "../components/createTemplate/CreateTemplate";
 
+export type Template = {
+    name: string;
+    weeks: Week[];
+}
 
 const BASE_URL = "http://localhost:5236/api/Templates";
+
+export async function createCalendarTemplate(eventTemplate: Template) {
+     {
+      const response = await fetch(
+        BASE_URL,
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+          body: JSON.stringify(eventTemplate),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+  
+  }
