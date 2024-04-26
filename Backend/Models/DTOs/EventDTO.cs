fb8336b76f997d7c851ca7bfdc58f41b6bbb0e85
Ewy Rostam <ewy.rost@gmail.com>
2024-04-26 09:19:56 +0200

    Create DTOs

---
+++ Backend/Models/DTOs/EventDTO.cs
@@ -0,0 +1,12 @@
+namespace Backend.Models.DTOs
+{
+    public class EventDTO
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int Day { get; set; }
+        public string? StartTime { get; set; }
+        public string? EndTime { get; set; }
+        public string? Recurrence { get; set; }
+    }
+}
